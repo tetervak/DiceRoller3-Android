40f407f74cb7f01e9b10ac4df4b4de193e5403b4
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-01-16 21:55:54 -0500

    defined the observable game in RollerViewModel

--- app/src/main/java/ca/tetervak/diceroller3/ui/roller/RollerViewModel.kt
+++ app/src/main/java/ca/tetervak/diceroller3/ui/roller/RollerViewModel.kt
@@ -1,5 +1,7 @@
 package ca.tetervak.diceroller3.ui.roller
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ca.tetervak.diceroller3.domain.Game
 import ca.tetervak.diceroller3.domain.asRollData
@@ -10,13 +12,17 @@ class RollerViewModel : ViewModel() {
     private val repository = GameDataRepository.getRepository()
 
     val gameValue: Game = Game()
+    private val _game: MutableLiveData<Game> = MutableLiveData(gameValue)
+    val game: LiveData<Game> = _game
 
     fun roll() {
         gameValue.roll()
+        _game.value = gameValue
     }
 
     fun reset() {
         gameValue.reset()
+        _game.value = gameValue
     }
 
     fun save(): Boolean {
